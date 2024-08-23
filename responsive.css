d34ae328e9eff25520e280f7af39aabc546469ba
mehaknegi <mehaknegi09@gmail.com>
2024-08-23 22:48:24 +0530

    Add files via upload

---
+++ responsive.css
@@ -0,0 +1,72 @@
+body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.image-container {
+    width: 625px;
+    height: 625px;
+    overflow: hidden;
+}
+
+.responsive-image {
+    position: relative;
+    width: 1250px;
+    height: 1250px;
+    object-fit: none;
+    object-position: top left;
+}
+
+/* Greater than 1600px - Top-left of homer.jpg */
+@media (min-width: 1601px) {
+    .responsive-image {
+        object-position: top left;
+    }
+}
+
+/* 1400px - 1600px - Top-right of homer.jpg */
+@media (min-width: 1401px) and (max-width: 1600px) {
+    .responsive-image {
+        object-position: top right;
+    }
+}
+
+/* 1200px - 1400px - Bottom-left of homer.jpg */
+@media (min-width: 1201px) and (max-width: 1400px) {
+    .responsive-image {
+        object-position: bottom left;
+    }
+}
+
+/* 1000px - 1200px - Bottom-right of homer.jpg */
+@media (min-width: 1001px) and (max-width: 1200px) {
+    .responsive-image {
+        object-position: bottom right;
+    }
+}
+
+/* 800px - 1000px - Bottom-left of homer.jpg */
+@media (min-width: 801px) and (max-width: 1000px) {
+    .responsive-image {
+        object-position: bottom left;
+    }
+}
+
+/* 600px - 800px - Top-right of homer.jpg */
+@media (min-width: 601px) and (max-width: 800px) {
+    .responsive-image {
+        object-position: top right;
+    }
+}
+
+/* Less than or equal to 600px - Top-left of homer.jpg */
+@media (max-width: 600px) {
+    .responsive-image {
+        object-position: top left;
+    }
+}
+
